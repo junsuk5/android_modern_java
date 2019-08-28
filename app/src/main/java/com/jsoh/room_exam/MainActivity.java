0342a8e8a28117990ce2281b01bed140756ea03b
Junsuk Oh <a811219@gmail.com>
2019-08-28 10:50:50 +0900

    DataBinding

--- app/src/main/java/com/jsoh/room_exam/MainActivity.java
+++ app/src/main/java/com/jsoh/room_exam/MainActivity.java
@@ -1,36 +1,24 @@
 package com.jsoh.room_exam;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.jsoh.room_exam.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText mTodoEditText;
-    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTodoEditText = findViewById(R.id.todo_edit);
-        mResultTextView = findViewById(R.id.result_text);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
 
         MainViewModel viewModel = ViewModelProviders.of(this)
                 .get(MainViewModel.class);
-
-        // UI 갱신
-        viewModel.getAll().observe(this, todos -> {
-            mResultTextView.setText(todos.toString());
-        });
-
-        // 버튼 클릭시 DB에 insert
-        findViewById(R.id.add_button).setOnClickListener(view -> {
-            viewModel.insert(new Todo(mTodoEditText.getText().toString()));
-        });
+        binding.setViewModel(viewModel);
     }
 
 }
--- app/src/main/java/com/jsoh/room_exam/MainViewModel.java
+++ app/src/main/java/com/jsoh/room_exam/MainViewModel.java
@@ -12,20 +12,23 @@ import java.util.List;
 
 public class MainViewModel extends AndroidViewModel {
     private AppDatabase db;
+    public LiveData<List<Todo>> todos;
+    public String newTodo;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
 
         db = Room.databaseBuilder(application, AppDatabase.class, "todo-db")
                 .build();
+        todos = getAll();
     }
 
     public LiveData<List<Todo>> getAll() {
         return db.todoDao().getAll();
     }
 
-    public void insert(Todo todo) {
-        new InsertAsyncTask(db.todoDao()).execute(todo);
+    public void insert(String todo) {
+        new InsertAsyncTask(db.todoDao()).execute(new Todo(todo));
     }
 
 
